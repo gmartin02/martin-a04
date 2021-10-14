b03eda1985773508cd77f6beefb47378406b5e97
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-13 23:58:42 -0400

    exercise43 initial logic added

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -4,29 +4,54 @@
  */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution43 {
+    private static final Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         WebsiteCreator wc = new WebsiteCreator();
+
         //calls all the Website Creator methods
+        wc.createHTMLFile();
     }
 
     public String getSiteName() {
         //prompts for site name
+        System.out.print("Site name: ");
         //returns site name
+        return input.nextLine();
     }
 
     public String getSiteAuthor() {
         //prompts for site author
+        System.out.print("Author: ");
         //returns site author
+        return input.nextLine();
     }
 
     public boolean javaFolderAnswer() {
         //prompts if user wants a java folder
+        System.out.print("Do you want a folder for JavaScript(Y/N)? ");
         //returns true for yes, false for no
+        String answer = input.nextLine();
+        if(answer.equals("Y")) {
+            return true;
+        } else if (answer.equals("N")) {
+            return false;
+        }
+        return false;
     }
 
     public boolean cssFolderAnswer() {
         //prompts if user wants a CSS folder
+        System.out.print("Do you want a folder for CSS(Y/N)? ");
         //returns true for yes, false for no
+        String answer = input.nextLine();
+        if(answer.equals("Y")) {
+            return true;
+        } else if (answer.equals("N")) {
+            return false;
+        }
+        return false;
     }
 }
--- exercise43/src/main/java/baseline/WebsiteCreator.java
+++ exercise43/src/main/java/baseline/WebsiteCreator.java
@@ -4,20 +4,59 @@
  */
 package baseline;
 
-public class WebsiteCreator {
-    Solution43 solution = new Solution43();
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
-    public void createHTMLFile(String siteName, String author) {
+public class WebsiteCreator {
+    String output = "Created ./website/";
+    String website = "website/";
+    Solution43 solution = new Solution43();
+    String siteName = solution.getSiteName();
+    String author = solution.getSiteAuthor();
+    boolean js = solution.javaFolderAnswer();
+    boolean css = solution.cssFolderAnswer();
+
+    public void createHTMLFile() {
         //takes siteName and Author from the main Solution class
+        File file = new File(website + siteName);
+        boolean bool = file.mkdir();
+        if(bool) {
+            System.out.println(output + siteName);
+        }
         //creates a html file with the title and author associated with the appropriate tags
-        //writes the html file and places it in the website directory
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("website/" + siteName + "/index.html"))) {
+            bw.write("<!DOCTYPE html><html><head><title>" + siteName + "</title><meta name = \"author\" content = \"" + author + "\"></head></html>");
+            //writes the html file and places it in the website directory
+            System.out.println(output + siteName + "/index.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(js) {
+            createJavaFolder();
+        }
+
+        if(css) {
+            createCSSFolder();
+        }
     }
 
     public void createJavaFolder() {
         //creates an empty java folder
+        File file = new File(website + siteName + "/js");
+        boolean bool = file.mkdirs();
+        if(bool) {
+            System.out.println(output + siteName + "/js");
+        }
     }
 
     public void createCSSFolder() {
         //creates an empty CSS folder
+        File file = new File(website + siteName + "/css");
+        boolean bool = file.mkdirs();
+        if(bool) {
+            System.out.println(output + siteName + "/css");
+        }
     }
 }
