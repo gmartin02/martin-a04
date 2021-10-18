78383af0933f8038be04c6fea41101177bbe181c
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-10-17 22:36:46 -0400

    exercise46 skeleton

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,32 @@
+package baseline;
+
+import java.util.List;
+import java.util.Map;
+
+public class Solution46 {
+    public String getFileData() {
+        //create a buffered reader insider a try/catch block
+            //while the next line is not null
+                //use concatentation to add that line to the String
+        //return the created String
+    }
+
+    public Map<String, Integer> countInstancesofWords() {
+
+        //split the string into a string array
+        //loop through the string array
+            //add each word to the array list
+        //sort the array list
+        //loop through the array list
+            //when it finds a word it will add it to the map as the key and increment the value
+            //if the word is already in the map it will just increment the value
+        //return the new map
+    }
+
+    public void writeToFile(Map<String, Integer> countedWords) {
+        //create a buffered writer
+            //create a buffered reader insider a try/catch block
+                //for loop through the size of the map
+                    //for each key print out its frequency with asterisks
+    }
+}
---
+++ exercise46/src/test/java/baseline/Solution46Test.java
@@ -0,0 +1,20 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution46Test {
+
+    @Test
+    void getFileData() {
+    }
+
+    @Test
+    void countInstancesofWords() {
+    }
+
+    @Test
+    void writeToFile() {
+    }
+}
